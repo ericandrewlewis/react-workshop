700e707df73131033e650ef96628802e97a74c11
Eric Lewis <eric.andrew.lewis@gmail.com>
2017-07-09 17:48:58 -0400

    Add class exercise

--- 01-es6-fundamentals/main.js
+++ 01-es6-fundamentals/main.js
@@ -25,3 +25,15 @@ function talkLikeMatthewMcConaughey() {
 }
 
 talkLikeMatthewMcConaughey();
+
+// EXERCISE: Add a bark function to the Dog class which returns "woof woof"
+// Log the output of a dog barking to the console.
+//
+class Dog {
+  render() {
+    return `▼・ᴥ・▼`;
+  }
+}
+
+const sparky = new Dog();
+console.log( sparky.render() );
