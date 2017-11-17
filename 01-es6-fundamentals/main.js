92cd90200c4295724f7dbc52ed4cc08ab4b72954
Eric Lewis <eric.andrew.lewis@gmail.com>
2017-11-16 19:38:42 -0500

    Update to React 16, small content updates

--- 01-es6-fundamentals/main.js
+++ 01-es6-fundamentals/main.js
@@ -1,31 +1,3 @@
-// EXERCISE:
-//
-// Step 1: Extract the year of the film with destructuring syntax
-// and print it with the other movie information.
-//
-// Step 2: Change the string to a template literal.
-function printMovieInfo(movie) {
-  const { title, star } = movie;
-  console.log(title + " starring " + star);
-}
-
-printMovieInfo({
-  title: 'Jurrassic Park',
-  year: '1993',
-  star: 'Laura Dern'
-});
-
-// EXERCISE: convert the variable definitions in this function
-// to use the const and let keywords instead of var.
-function talkLikeMatthewMcConaughey() {
-  var alright = 'alright';
-  for (var i = 0; i <= 2; i++) {
-    console.log(alright);
-  }
-}
-
-talkLikeMatthewMcConaughey();
-
 // EXERCISE: Add a bark function to the Dog class which returns "woof woof"
 // Log the output of a dog barking to the console.
 //
--- 03-create-a-custom-component/main.js
+++ 03-create-a-custom-component/main.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-// EXERCISE
-//
-// Create a own component class for a <Clock> which returns a hardcoded
-// time (eg. 3:10 or 7:15) and add it into the component tree.
-
-
 // We can create custom component types by creating a child component
 // of React's Component class.
 //
@@ -16,20 +10,25 @@ class SiteHeader extends Component {
   render() {
     return (
       <div>
-        <h1>Frank Ocean Fan Website</h1>
+        <h1>Brian Eno Fan Website</h1>
         <div>
-          <img src="http://thefader-res.cloudinary.com/images/w_1440,c_limit,f_auto,q_auto:best/gizfef3m2peiqleuyr7x/frank-ocean.jpg" width="400" />
+          <img src="http://www.saltartists.com/wp-content/uploads/2014/05/Brian-Eno-04.jpg" width="400" />
         </div>
         <nav>
           <ul>
-            <li><a href="about">Frank&#39;s Albums</a></li>
-            <li><a href="contact-us">Pictures of Frank</a></li>
+            <li><a href="about">Brian&#39;s Albums</a></li>
+            <li><a href="contact-us">More Pictures of Brian</a></li>
           </ul>
         </nav>
       </div>
     );
   }
 }
+
+// EXERCISE
+//
+// Create a own component class for a <Clock> which returns a hardcoded
+// time (eg. 3:10 or 7:15) and add it into the component tree.
 
 ReactDOM.render(
   <div>
